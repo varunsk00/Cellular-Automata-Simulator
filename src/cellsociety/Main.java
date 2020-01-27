d8dc8ed6c5828e2d3e97d8f4f8665055d462e4b8
Jaidha Rosenblatt <jrr59@duke.edu>
2020-01-26 20:15:11 -0500

    setup stage and animation loop in main

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,13 +1,46 @@
 package cellsociety;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
-public class Main {
+public class Main extends Application {
     /**
      * Start of the program.
      */
-    public static void main (String[] args) {
-        System.out.println("Hello world");
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    public static double SCENE_WIDTH = 400;
+    public static double SCENE_HEIGHT = 400;
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Simulation");
+        startAnimationLoop();
+
+        Group root = new Group();
+        Scene scene = new Scene(root, SCENE_WIDTH,SCENE_HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void startAnimationLoop(){
+        KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
+
+    private void step(double elapsedTime){
+
     }
 }
