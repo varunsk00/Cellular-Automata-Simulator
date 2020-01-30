1528fceaafdc5aca9db9558e8b22a9dff2ab6d89
Varun Kosgi <vsk10@duke.edu>
2020-01-29 23:39:19 -0500

    Resizing:

--- src/cellsociety/Grid.java
+++ src/cellsociety/Grid.java
@@ -31,10 +31,17 @@ public class Grid {
    * Initializes an ArrayList of ArrayLists representative of the grid
    **/
   private void createGrid() {
-    for (int i = 0; i < rows; i++) {
+    for(int i = 0; i<rows; i++){
       ArrayList<Cell> row = new ArrayList<>();
-      for (int j = 0; j < columns; j++) {
-        row.add(new Cell(Color.WHITE, "empty"));
+      for(int j = 0; j<columns; j++){
+        if (i % 2 == 0) {
+          System.out.println("full");
+          row.add(new Cell(Color.BLACK, "water"));
+        }
+        else{
+          System.out.println("empty");
+          row.add(new Cell(Color.WHITE, "empty"));
+        }
       }
       grid.add(row);
     }
@@ -63,50 +70,50 @@ public class Grid {
 
   /**
    * Checks every cell in the current grid and updates based on state of neighbors
-   *
    * @return a grid (2D array of cells) with updated state
    */
-  public void updateGrid() {
-    ArrayList<ArrayList<Cell>> newGrid = grid;
+  public Cell[][] updateGrid() {
+    int rows = grid.size();
+    int columns = grid.get(0).size();
+    Cell[][] newGrid = new Cell[rows][columns];
 
-    for (ArrayList<Cell> row : newGrid) {
-      for (Cell cell : row) {
-        int x = grid.indexOf(row);
-        int y = row.indexOf(cell);
-        if(x%2==0 && y%2!=0){
-          cell.update(Color.BLACK, "full");
-        }
-        if(x%2!=0 && y%2==0){
-          cell.update(Color.BLACK, "full");
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        //TODO is a try/catch loop better?
+        if (i > 0 && i < rows - 1 && j > 0 && j < columns - 1) {
+          updateMiddleCell(i, j);
+        } else {
+          updateEdgeCell(i, j);
         }
       }
     }
-    this.grid = newGrid;
+    return newGrid;
   }
 
-//  private Cell updateMiddleCell(int x, int y) {
-//    Cell cell = grid.get(x).get(y);
-//    String state = "burning";
-//    //Check cell neighbors
-//    for (int i = x - 1; i < x + 1; i++) {
-//      for (int j = y - 1; j < y + 1; j++) {
-//        if (getCellState(i, j) == state) {
-//          cell.update(Color.RED, "burning");
-//        }
-//      }
-//    }
-//
-//    return cell;
-//  }
+  private Cell updateMiddleCell(int x, int y) {
+    Cell cell = grid.get(x).get(y);
+    String state = "burning";
+    //Check cell neighbors
+    for (int i = x - 1; i < x + 1; i++) {
+      for (int j = y - 1; j < y + 1; j++) {
+        if (getCellState(i, j) == state) {
+          cell.update(Color.RED, "burning");
+        }
+      }
+    }
 
-//  private Cell updateEdgeCell(int x, int y) {
-//    //TODO
-//    Cell cell = grid.get(x).get(y);;
-//    return cell;
-//  }
-//
-//  private String getCellState(int i, int j) {
-//    Cell cell = grid.get(i).get(j);;
-//    return cell.getState();
-//  }
+    return cell;
+  }
+
+  private Cell updateEdgeCell(int x, int y) {
+    //TODO
+    Cell cell = grid.get(x).get(y);;
+    return cell;
+  }
+
+  private String getCellState(int i, int j) {
+    Cell cell = grid.get(i).get(j);;
+    return cell.getState();
+  }
+
 }
