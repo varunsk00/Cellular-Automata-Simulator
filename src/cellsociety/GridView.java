9a81a743acb70681b5aa8ea9bb90c27e948da933
Varun Kosgi <vsk10@duke.edu>
2020-01-29 23:45:34 -0500

    Git error

--- src/cellsociety/GridView.java
+++ src/cellsociety/GridView.java
@@ -9,7 +9,8 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class GridView {
-
+  private double scene_height;
+  private double scene_width;
   /**
    * Calls renderGrid() to turn grid into a collection of shape objects that we can add to the root
    * of our scene
@@ -18,17 +19,18 @@ public class GridView {
    * @param sceneHeight the height of the scene we want to return. C
    * @return a scene that is a rendered version of our grid
    **/
-  public Scene getScene(Grid grid, double sceneWidth, double sceneHeight) {
-    Group root = new Group();
+  public Group createGroup(Grid grid, double sceneWidth, double sceneHeight) {
+    scene_width = sceneWidth;
+    scene_height = sceneHeight;
+    Group gridView = new Group();
 
     double cellWidth = sceneWidth / grid.getColumns();
     double cellHeight = sceneHeight / grid.getRows();
 
     Collection<Shape> cells = renderGrid(grid, cellWidth, cellHeight);
-    root.getChildren().addAll(cells);
+    gridView.getChildren().addAll(cells);
 
-    Scene scene = new Scene(root, sceneWidth, sceneHeight);
-    return scene;
+    return gridView;
   }
 
   /**
@@ -40,14 +42,14 @@ public class GridView {
    * @return a collection of cells transformed into shapes
    **/
   public Collection<Shape> renderGrid(Grid grid, double cellWidth,
-      double cellHeight) {
+                                      double cellHeight) {
     Collection<Shape> cells = new ArrayList<>();
 
     for (ArrayList<Cell> row : grid.getGrid()) {
       for (Cell cell : row) {
         int x = grid.getGrid().indexOf(row);
         int y = row.indexOf(cell);
-        Shape shape = new Rectangle(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+        Shape shape = new Rectangle(x * cellWidth, y * cellHeight+(scene_height/10), cellWidth, cellHeight);
         shape.setFill(cell.getColor());
         cells.add(shape);
       }
--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -3,9 +3,12 @@ package cellsociety;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.util.ArrayList;
 
 
 public class Main extends Application {
@@ -14,7 +17,9 @@ public class Main extends Application {
   private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
   private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
   private static double SCENE_WIDTH = 400;
-  private static double SCENE_HEIGHT = 400;
+  private static double SCENE_HEIGHT = 500;
+
+  private Grid grid = new Grid(100, 100);
 
   /**
    * Begins our JavaFX application Gets the current grid and sets the stage to a scene with that
@@ -23,13 +28,20 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
 
+    Group root = new Group();
+
     primaryStage.setTitle("Simulation");
     startAnimationLoop();
 
-    Grid grid = new Grid(100, 100);
+    grid.updateGrid();
     GridView gridView = new GridView();
-    Scene scene = gridView.getScene(grid, SCENE_WIDTH, SCENE_HEIGHT);
+    Group gridGroup = gridView.createGroup(grid, SCENE_WIDTH, SCENE_HEIGHT - (SCENE_HEIGHT/10));
 
+
+    Visualizer GUIControl = new Visualizer(SCENE_WIDTH, SCENE_HEIGHT);
+    root.getChildren().addAll(GUIControl.createSimulator(), gridGroup);
+
+    Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
     primaryStage.setScene(scene);
     primaryStage.show();
   }
