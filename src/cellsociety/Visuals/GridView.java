bc3d4a173a41fcc830f5196f51350bf813c0e6bf
Eric Doppelt <ead45@duke.edu>
2020-02-01 15:27:31 -0500

    Moved GridVieww to Visuals

--- src/cellsociety/Visuals/GridView.java
+++ src/cellsociety/Visuals/GridView.java
@@ -1,8 +1,10 @@
-package cellsociety;
+package cellsociety.Visuals;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import cellsociety.Cell;
+import cellsociety.Grid;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
