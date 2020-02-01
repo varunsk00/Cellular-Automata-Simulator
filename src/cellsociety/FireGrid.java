617068f583017f9b911b88580e130b13f882f44d
Varun Kosgi <vsk10@duke.edu>
2020-02-01 18:31:10 -0500

    Added probability of catch

--- src/cellsociety/FireGrid.java
+++ src/cellsociety/FireGrid.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class FireGrid extends Grid {
+    private double probability;
+    ArrayList<Integer> nums = new ArrayList<>();
     /**
      * Sets rows and columns and instance variables Calls createGrid to initialize a grid of cells
      * based on given rows and columns
@@ -13,8 +15,9 @@ public class FireGrid extends Grid {
      * @param rows    the number of rows to generate in our grid
      * @param columns the number of columns to generate in our grid
      **/
-    public FireGrid(int rows, int columns) {
+    public FireGrid(int rows, int columns, double probCatch) {
         super(rows, columns);
+        this.probability = probCatch;
     }
 
     @Override
@@ -23,12 +26,18 @@ public class FireGrid extends Grid {
         for (int i = 0; i < getRows(); i++) {
             ArrayList<Cell> row = new ArrayList<>();
             for (int j = 0; j < getColumns(); j++) {
-                row.add(new Cell(Color.GREEN, "tree"));
+                if (i==0||j==0||i==getRows()-1||j==getColumns()-1){
+                    row.add(new Cell(Color.YELLOW, "empty"));
+                }
+                else {
+                    row.add(new Cell(Color.GREEN, "tree"));
+                }
             }
             ret.add(row);
         }
         return ret;
     }
+    //TODO REFACTOR
     @Override
     public void updateGrid() throws InterruptedException {
         Random r = new Random();
@@ -37,8 +46,10 @@ public class FireGrid extends Grid {
             for (Cell cell : row) {
 
 
-                int random_x = r.nextInt(getColumns());
-                int random_y = r.nextInt(getRows());
+                int prob = r.nextInt(10);
+                for(int i = 0; i< (probability*10); i++){
+                    nums.add(i);
+                }
                 //getGrid().get(random_x).get(random_y).update(Color.RED, "burning");
                 //System.out.println("lit: " + (random_x) + ", " + (random_y));
                 //Thread.sleep(100);
@@ -46,8 +57,19 @@ public class FireGrid extends Grid {
                 int x = getGrid().indexOf(row);
                 int y = row.indexOf(cell);
                 if(x>0 && y>0 && x<getColumns()-1 && y<getRows()-1){
-                    if(newGrid.get(x).get(y+1).getState() == "burning"|| newGrid.get(x).get(y-1).getState() == "burning" ||
-                            newGrid.get(x-1).get(y).getState() == "burning" || newGrid.get(x+1).get(y).getState() == "burning"){
+                    if(newGrid.get(x).get(y+1).getState() == "burning" && nums.contains(prob) ){
+                        cell.update(Color.RED, "burning");
+                        System.out.println("caught fire: " + (x) + ", " + (y));
+                    }
+                    if(newGrid.get(x).get(y-1).getState() == "burning" && nums.contains(prob) ){
+                        cell.update(Color.RED, "burning");
+                        System.out.println("caught fire: " + (x) + ", " + (y));
+                    }
+                    if(newGrid.get(x-1).get(y).getState() == "burning" && nums.contains(prob) ){
+                        cell.update(Color.RED, "burning");
+                        System.out.println("caught fire: " + (x) + ", " + (y));
+                    }
+                    if(newGrid.get(x+1).get(y).getState() == "burning" && nums.contains(prob) ){
                         cell.update(Color.RED, "burning");
                         System.out.println("caught fire: " + (x) + ", " + (y));
                     }
