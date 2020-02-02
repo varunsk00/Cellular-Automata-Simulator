536b29f9c6ce381f4ec66729d68cac4f6fde499a
Varun Kosgi <vsk10@duke.edu>
2020-02-01 20:39:40 -0500

    Added Fire spread test case

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -15,7 +15,7 @@ import javafx.scene.paint.Color;
 
 public class Main extends Application {
 
-  private static final double FRAMES_PER_SECOND = 2;
+  private static final double FRAMES_PER_SECOND = 30;
   private static final double MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
   private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
   private static double SCENE_WIDTH = 400;
@@ -37,8 +37,8 @@ public class Main extends Application {
     primaryStage.setTitle("Simulation");
     startAnimationLoop();
 
-    grid = new FireGrid(100,100, 0.4);
-    grid.getGrid().get(98).get(98).update(Color.RED, "burning");
+    grid = new FireGrid(100,100, 0.6);
+    grid.getGrid().get(grid.getRows()/2).get(grid.getColumns()/2).update(Color.RED, "burning");
 
     GUIController = new Header(SCENE_WIDTH, "Standard");
     gridView = new GridView(grid,SCENE_WIDTH,SCENE_HEIGHT);
