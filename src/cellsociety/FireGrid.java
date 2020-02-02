a07e8f7fc04019df6487877aeedf4238c9b09647
Varun Kosgi <vsk10@duke.edu>
2020-02-01 20:20:23 -0500

    added burn->empty, refactoring

--- src/cellsociety/FireGrid.java
+++ src/cellsociety/FireGrid.java
@@ -1,13 +1,13 @@
 package cellsociety;
-
 import javafx.scene.paint.Color;
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class FireGrid extends Grid {
     private double probability;
-    ArrayList<Integer> nums = new ArrayList<>();
+    private ArrayList<Integer> nums = new ArrayList<>();
+    private int prob;
+    private Random r = new Random();
     /**
      * Sets rows and columns and instance variables Calls createGrid to initialize a grid of cells
      * based on given rows and columns
@@ -18,6 +18,9 @@ public class FireGrid extends Grid {
     public FireGrid(int rows, int columns, double probCatch) {
         super(rows, columns);
         this.probability = probCatch;
+        for (int i = 0; i < (probability * 10); i++) {
+            this.nums.add(i);
+        }
     }
 
     @Override
@@ -26,10 +29,10 @@ public class FireGrid extends Grid {
         for (int i = 0; i < getRows(); i++) {
             ArrayList<Cell> row = new ArrayList<>();
             for (int j = 0; j < getColumns(); j++) {
-                if (i==0||j==0||i==getRows()-1||j==getColumns()-1){
+                if (i == 0 || j == 0 || i == getRows() - 1 || j == getColumns() - 1){
                     row.add(new Cell(Color.YELLOW, "empty"));
                 }
-                else {
+                else{
                     row.add(new Cell(Color.GREEN, "tree"));
                 }
             }
@@ -37,46 +40,35 @@ public class FireGrid extends Grid {
         }
         return ret;
     }
-    //TODO REFACTOR
+
     @Override
-    public void updateGrid() throws InterruptedException {
-        Random r = new Random();
-        ArrayList<ArrayList<Cell>> newGrid = getGrid();
-        for (ArrayList<Cell> row : newGrid) {
+    public void updateGrid(){
+        for (ArrayList<Cell> row : getGrid()) {
             for (Cell cell : row) {
-
-
-                int prob = r.nextInt(10);
-                for(int i = 0; i< (probability*10); i++){
-                    nums.add(i);
-                }
-                //getGrid().get(random_x).get(random_y).update(Color.RED, "burning");
-                //System.out.println("lit: " + (random_x) + ", " + (random_y));
-                //Thread.sleep(100);
-
                 int x = getGrid().indexOf(row);
                 int y = row.indexOf(cell);
-                if(x>0 && y>0 && x<getColumns()-1 && y<getRows()-1){
-                    if(newGrid.get(x).get(y+1).getState() == "burning" && nums.contains(prob) ){
-                        cell.update(Color.RED, "burning");
-                        System.out.println("caught fire: " + (x) + ", " + (y));
-                    }
-                    if(newGrid.get(x).get(y-1).getState() == "burning" && nums.contains(prob) ){
-                        cell.update(Color.RED, "burning");
-                        System.out.println("caught fire: " + (x) + ", " + (y));
-                    }
-                    if(newGrid.get(x-1).get(y).getState() == "burning" && nums.contains(prob) ){
-                        cell.update(Color.RED, "burning");
-                        System.out.println("caught fire: " + (x) + ", " + (y));
-                    }
-                    if(newGrid.get(x+1).get(y).getState() == "burning" && nums.contains(prob) ){
-                        cell.update(Color.RED, "burning");
-                        System.out.println("caught fire: " + (x) + ", " + (y));
-                    }
-                }
-                //
+                handleBurningCell(x,y);
+                handleMiddleCell(x,y);
             }
         }
     }
-}
 
+    @Override
+    public void handleMiddleCell(int x, int y){
+        prob = r.nextInt(10);
+        if (getGrid().get(x).get(y).getState() == "tree" && nums.contains(prob) &&
+                                   (getGrid().get(x).get(y + 1).getState() == "burning" ||
+                                    getGrid().get(x).get(y - 1).getState() == "burning" ||
+                                    getGrid().get(x + 1).get(y).getState() == "burning" ||
+                                    getGrid().get(x - 1).get(y).getState() == "burning")) {
+            getGrid().get(x).get(y).update(Color.RED, "burning");
+            System.out.println("caught fire: " + (x) + ", " + (y));
+        }
+    }
+
+    public void handleBurningCell(int x, int y){
+        if (getGrid().get(x).get(y).getState() == "burning") {
+            getGrid().get(x).get(y).update(Color.YELLOW, "empty");
+        }
+    }
+}
