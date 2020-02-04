df6beb9e9d61e5f87110d7920675272fafffce90
Varun Kosgi <vsk10@duke.edu>
2020-02-04 03:04:34 -0500

    Refactored edge cases

--- src/cellsociety/Grids/LifeGrid.java
+++ src/cellsociety/Grids/LifeGrid.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Random;
 
 import cellsociety.Cell;
-import cellsociety.Grids.Grid;
 import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
@@ -28,7 +27,7 @@ public class LifeGrid extends Grid {
      * @param rows    the number of rows to generate in our grid
      * @param columns the number of columns to generate in our grid
      **/
-    public LifeGrid(int rows, int columns, double percentAlive) {
+  public LifeGrid(int rows, int columns, double percentAlive) {
         super(rows, columns);
         this.percentAlive = percentAlive;
         this.aliveCells = new ArrayList<Point>();
@@ -51,127 +50,68 @@ public class LifeGrid extends Grid {
       }
     }
   }
-    @Override
-    public ArrayList<ArrayList<Cell>> createGrid() {
-        ArrayList<ArrayList<Cell>> ret = new ArrayList<>();
-        for (int i = 0; i < getRows(); i++) {
-            ArrayList<Cell> row = new ArrayList<>();
-            for (int j = 0; j < getColumns(); j++) {
-                row.add(new Cell(Color.WHITE, "dead"));
-            }
-            ret.add(row);
-        }
-        return ret;
-    }
 
-    @Override
-    public void updateGrid(){
-        storeNeigborState(aliveCells, "alive");
-        for (ArrayList<Cell> row : getGrid()) {
-            for (Cell cell : row) {
-                int x = getGrid().indexOf(row);
-                int y = row.indexOf(cell);
-                if (isMiddleCell(x, y)) {
-                    handleMiddleCell(x, y);
-                } else {
-                    handleEdgeCell(x, y);
-                }
-            }
-        }
-    }
+  @Override
+  public ArrayList<ArrayList<Cell>> createGrid() {
+      ArrayList<ArrayList<Cell>> ret = new ArrayList<>();
+      for (int i = 0; i < getRows(); i++) {
+          ArrayList<Cell> row = new ArrayList<>();
+          for (int j = 0; j < getColumns(); j++) {
+              row.add(new Cell(Color.WHITE, "dead"));
+          }
+          ret.add(row);
+      }
+      return ret;
+  }
 
-    @Override
-    public void handleMiddleCell(int x, int y){
-        ArrayList<Cell> neighbors = getAllNeighbors(x,y);
-        int alive_count= 0;
-        for (Cell c: neighbors){
-            if (c.getState() == "alive"){
+  @Override
+  public void updateGrid(){
+      storeNeigborState(aliveCells, "alive");
+      for (ArrayList<Cell> row : getGrid()) {
+          for (Cell cell : row) {
+              int x = getGrid().indexOf(row);
+              int y = row.indexOf(cell);
+              if (isMiddleCell(x, y)) {
+                  handleMiddleCell(x, y);
+              } else {
+                  handleEdgeCell(x, y);
+              }
+          }
+      }
+  }
+
+  @Override
+  public void handleMiddleCell(int x, int y){
+      ArrayList<Cell> neighbors = getAllNeighbors(x,y);
+      int alive_count= 0;
+      for (Cell c: neighbors){
+          if (c.getState() == "alive"){
                 alive_count++;
-            }
-        }
+          }
+      }
+      if (checkNeighbors(x, y, aliveCells) && current(x,y).getState().equals("alive")){
+          if(alive_count==2 || alive_count ==3){
+              current(x,y).update(Color.BLACK, "alive");
+              System.out.println("survived: " + (x) + ", " + (y));
+          }
+          else{
+              current(x,y).update(Color.WHITE, "dead");
+              System.out.println("died: " + (x) + ", " + (y));
+          }
+      }
+      else{
+          if (alive_count==3){
+              current(x,y).update(Color.BLACK, "alive");
+              System.out.println("reproduced: " + (x) + ", " + (y));
+          }
+          else{
+              current(x,y).update(Color.WHITE, "dead");
+          }
+      }
+  }
 
-        if (checkNeighbors(x, y, aliveCells) && current(x,y).getState().equals("alive")){
-            if(alive_count==2 || alive_count ==3){
-                current(x,y).update(Color.BLACK, "alive");
-                System.out.println("survived: " + (x) + ", " + (y));
-            }
-            else{
-                current(x,y).update(Color.WHITE, "dead");
-                System.out.println("died: " + (x) + ", " + (y));
-            }
-        }
-        else{
-            if (alive_count==3){
-                current(x,y).update(Color.BLACK, "alive");
-                System.out.println("reproduced: " + (x) + ", " + (y));
-            }
-            else{
-                current(x,y).update(Color.WHITE, "dead");
-            }
-        }
-    }
-
-    public void handleEdgeCell(int x, int y){
-        ArrayList<Cell> neighbors = new ArrayList<>();
-        if(y==0){
-            if(x==0){
-                neighbors.add(getGrid().get(x+1).get(y+1));
-                neighbors.add(getGrid().get(x).get(y+1));
-                neighbors.add(getGrid().get(x+1).get(y));
-            }
-            else if (x==getColumns()-1){
-                neighbors.add(getGrid().get(x-1).get(y));
-                neighbors.add(getGrid().get(x-1).get(y+1));
-                neighbors.add(getGrid().get(x).get(y+1));
-            }
-            else{
-                neighbors.add(getGrid().get(x-1).get(y));
-                neighbors.add(getGrid().get(x-1).get(y+1));
-                neighbors.add(getGrid().get(x).get(y+1));
-                neighbors.add(getGrid().get(x+1).get(y+1));
-                neighbors.add(getGrid().get(x).get(y+1));
-                neighbors.add(getGrid().get(x+1).get(y));
-            }
-        }
-        if(y==getRows()-1){
-            if(x==0){
-                neighbors.add(getGrid().get(x).get(y-1));
-                neighbors.add(getGrid().get(x+1).get(y-1));
-                neighbors.add(getGrid().get(x+1).get(y));
-            }
-            else if (x==getColumns()-1){
-                neighbors.add(getGrid().get(x-1).get(y));
-                neighbors.add(getGrid().get(x-1).get(y-1));
-                neighbors.add(getGrid().get(x).get(y-1));
-            }
-            else{
-                neighbors.add(getGrid().get(x-1).get(y));
-                neighbors.add(getGrid().get(x-1).get(y-1));
-                neighbors.add(getGrid().get(x).get(y-1));
-                neighbors.add(getGrid().get(x+1).get(y-1));
-                neighbors.add(getGrid().get(x).get(y-1));
-                neighbors.add(getGrid().get(x+1).get(y));
-            }
-        }
-        if(x==0){
-            if(y!=0 && y!= getRows()-1){
-                neighbors.add(getGrid().get(x).get(y-1));
-                neighbors.add(getGrid().get(x+1).get(y-1));
-                neighbors.add(getGrid().get(x+1).get(y));
-                neighbors.add(getGrid().get(x+1).get(y+1));
-                neighbors.add(getGrid().get(x).get(y+1));
-            }
-        }
-        if(x==getColumns()-1){
-            if(y!=0 && y!= getRows()-1){
-                neighbors.add(getGrid().get(x).get(y-1));
-                neighbors.add(getGrid().get(x-1).get(y-1));
-                neighbors.add(getGrid().get(x-1).get(y));
-                neighbors.add(getGrid().get(x-1).get(y+1));
-                neighbors.add(getGrid().get(x).get(y+1));
-            }
-        }
-
+  public void handleEdgeCell(int x, int y){
+        ArrayList<Cell> neighbors = handleEdgeCases(x,y);
         int alive_count= 0;
         for (Cell c: neighbors){
             if (c.getState() == "alive"){
