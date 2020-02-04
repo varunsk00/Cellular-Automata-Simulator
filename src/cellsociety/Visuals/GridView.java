38421dc7814a79030b00b61bf74fe726ed4a7f24
Eric Doppelt <ead45@duke.edu>
2020-02-03 20:57:27 -0500

    Minor Refactoringg, deleted commented code beneath

--- src/cellsociety/Visuals/GridView.java
+++ src/cellsociety/Visuals/GridView.java
@@ -29,7 +29,7 @@ public class GridView {
     for (int i = 0; i < grid.getColumns(); i++) {
       for (int j = 0; j < grid.getRows(); j++) {
         Region addedShape = new Region();
-        addedShape.setBackground(new Background(new BackgroundFill(grid.getGrid().get(j).get(i).getColor(), CornerRadii.EMPTY, Insets.EMPTY)));
+        addedShape.setBackground(new Background(new BackgroundFill(grid.current(j, i).getColor(), CornerRadii.EMPTY, Insets.EMPTY)));
         myGridPane.add(addedShape, i, j);
         myGridPane.setHgrow(addedShape, Priority.ALWAYS);
         myGridPane.setVgrow(addedShape, Priority.ALWAYS);
@@ -37,28 +37,3 @@ public class GridView {
     }
   }
 }
-
-
-/*
-    for (int i = 0; i < grid.getColumns(); i++) {
-        for (int j = 0; j < grid.getRows(); j++) {
-        Color oldColor = grid.getGrid().get(j).get(i).getColor();
-        Paint newPaint = ((Region)myGridPane.getChildren().get(grid.getRows() * i + j)).getBackground().getFills().get(0).getFill();
-        Color newColor = (Color)newPaint;
-
-        if (!oldColor.equals(newColor)) {
-        System.out.println(i + ", " + j);
-        System.out.println(oldColor);
-        System.out.println(newColor);
-
-        myGridPane.getChildren().remove(grid.getRows() * i + j);
-        Region addedShape = new Region();
-        addedShape.setBackground(new Background(new BackgroundFill(grid.getGrid().get(j).get(i).getColor(), CornerRadii.EMPTY, Insets.EMPTY)));
-        myGridPane.getChildren().add(grid.getRows() * i + j, addedShape);
-        myGridPane.setHgrow(addedShape, Priority.ALWAYS);
-        myGridPane.setVgrow(addedShape, Priority.ALWAYS);
-        }
-        }
-        }
-
- */
