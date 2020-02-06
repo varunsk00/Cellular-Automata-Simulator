18629edb2a5f046080159b4c848154331f5370ed
Eric Doppelt <ead45@duke.edu>
2020-02-06 02:08:31 -0500

    Created basic stat class that holds title, author, and stats

--- src/cellsociety/Visuals/StatBox.java
+++ src/cellsociety/Visuals/StatBox.java
@@ -2,43 +2,47 @@ package cellsociety.Visuals;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
-import java.awt.*;
 import java.util.HashMap;
 
-public class StatColumn {
+public class StatBox {
 
     private static final String TITLE_SETUP = "Title: ";
+    private static final String AUTHOR_SETUP = "Author: ";
     private static final Color GRID_BACKGROUND = Color.DARKGOLDENROD;
     private static final BackgroundFill STAT_BACKGROUND = new BackgroundFill(GRID_BACKGROUND, CornerRadii.EMPTY, Insets.EMPTY);
     private VBox statBox;
     private HashMap<String, Double> myStats;
     private String myTitle;
+    private String myAuthor;
 
 
-    public StatColumn(String title, HashMap stats) {
+    public StatBox(String title, String author, HashMap stats) {
         myTitle = title;
+        myAuthor = author;
         myStats = stats;
         renderStatBox();
     }
 
     private void renderStatBox() {
         statBox = new VBox();
-        statBox.setBackground(new Background((STAT_BACKGROUND));
+        statBox.setBackground(new Background(STAT_BACKGROUND));
 
-        statBox.getChildren().add(new Label(TITLE_SETUP + myTitle));
+        Label authorLabel = new Label(AUTHOR_SETUP + myAuthor);
+        statBox.setVgrow(authorLabel, Priority.ALWAYS);
+        Label titleLabel = new Label(TITLE_SETUP + myTitle);
+        statBox.setVgrow(titleLabel, Priority.ALWAYS);
 
         for (String key : myStats.keySet()) {
             Label tempLabel = new Label(key + ": " + myStats.get(key));
-            statBox.setVgrow(tempLabel, true);
+            statBox.setVgrow(tempLabel, Priority.ALWAYS);
             statBox.getChildren().add(tempLabel);
         }
     }
 
-
+    public VBox getStatBox() {
+        return statBox;
+    }
 }
